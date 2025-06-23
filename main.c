c618a0d18bbf7569984f3c0cafb5724785f0248f
Phil Howard <github@gadgetoid.com>
2025-06-23 22:48:02 +0100

    Many tweaks, arg parsing, tidying up, more options.

--- main.c
+++ main.c
@@ -4,25 +4,114 @@
 #include <ftw.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <argp.h>
+#include <stdbool.h>
 
 #include "ff.h"
 #include "diskio.h"
 #include "ffconf-micropython.h"
 
-#define SECTOR_SIZE 4096
+// Run ./ffsmake directory output-file.bin
+// For a 2MB file on a 4MB flash, load with: picotool load -o 0x10200000 filesystem.bin
+// for macOS users: brew install argp-standalone
+
+#define DEFAULT_SECTOR_SIZE 4096
+#define DEFAULT_SECTOR_COUNT 512
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 
-#define FF_DEBUG(...) //printf
-#define DEBUG(...)
+#define MB 1024.0f / 1024.0f
+
+#define FF_DEBUG(...) args.debug ? printf(__VA_ARGS__) : 0
+#define DEBUG(...) args.debug ? printf(__VA_ARGS__) : 0
+#define VERBOSE(...) args.verbose ? printf(__VA_ARGS__) : 0
+
+FATFS fs;
+FIL fil;
+FRESULT fr;
+
+char copy_buffer[1024];
+char copy_path[8192]; // Ugh
+char output_file[8192]; // Also ugh
+
+const char *argp_program_version = "v0.0.2";
+const char *argp_program_bug_address = "<ffsmake@gadgetoid.com>";
+static char doc[] = "FatFS filesystem builder for MicroPython.";
+static char args_doc[] = "[FILENAME]...";
+static struct argp_option options[] = {
+    { "directory", 'd', "FILE", 0, "Input directory."},
+    { "output", 'o', "FILE", 0, "Output file for FatFS filesystem."},
+    { "sector-size", 's', "NUMBER", OPTION_ARG_OPTIONAL, "Sector size (default 4096 bytes)."},
+    { "sector-count", 'c', "NUMBER", OPTION_ARG_OPTIONAL, "Sector count (default 512, 2MB disk)."},
+    { "no-truncate", 't', 0, 0, "Do not truncate output file to used capacity."},
+    { "force", 'f', 0, 0, "Force overwrite an existing file."},
+    { "quiet", 'q', 0, 0, "No shouty output."},
+    { "debug", 'D', 0, 0, "Very shouty output."},
+    { 0 }
+};
+
+struct arguments
+{
+  char *args[2];                // Positional args
+  char *input_directory;
+  char *output_file;
+  bool truncate;
+  bool force;
+  bool verbose;
+  bool debug;
+  DWORD sector_count;
+  WORD sector_size;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct arguments *arguments = state->input;
+    char *end;
+    switch (key) {
+    case 'c':
+        arguments->sector_count = (DWORD)strtol(arg, &end, 10);
+        if(arg == end) {
+            argp_failure(state, 1, 0, "invalid value given for sector count.");
+        }
+        break;
+    case 's':
+        arguments->sector_size = (WORD)strtol(arg, &end, 10);
+        if(arg == end) {
+            argp_failure(state, 1, 0, "invalid value given for sector size.");
+        }
+        break;
+    case 'd': arguments->input_directory = arg; break;
+    case 'o': arguments->output_file = arg; break;
+    case 't': arguments->truncate = false; break;
+    case 'f': arguments->force = true; break;
+    case 'q': arguments->verbose = false; break;
+    case 'D': arguments->debug = true; break;
+    case ARGP_KEY_ARG:
+        arguments->args[state->arg_num] = arg;
+        return 0;
+    case ARGP_KEY_END:
+        if (arguments->input_directory == NULL || arguments->output_file == NULL){
+            argp_error(state, "requires both -d and -o.");
+        }
+        if (arguments->sector_count == 0 || arguments->sector_size == 0) {
+            argp_failure(state, 1, 0, "sector size and/or count must be non-zero.");
+        }
+        return 0;
+    default: return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
+struct arguments args;
 
 struct _bdev_t {
-    size_t sector_count, sector_size;
+    DWORD sector_count;
+    WORD sector_size;
     uint8_t *blocks;
     size_t used_bytes;
 };
 
 struct _bdev_t mkdisk(DWORD sector_size, DWORD sector_count) {
-    DEBUG("mkdisk(%u, %u) -> %d bytes\n", sector_size, sector_count, sector_size * sector_count);
+    DEBUG("-- mkdisk(%u, %u) -> %d bytes\n", sector_size, sector_count, sector_size * sector_count);
     struct _bdev_t bdev = {
         sector_count,
         sector_size,
@@ -30,30 +119,31 @@ struct _bdev_t mkdisk(DWORD sector_size, DWORD sector_count) {
         0
     };
     if(bdev.blocks == NULL) {
-        printf("mkdisk malloc failed\n");
+        DEBUG("mkdisk malloc failed\n");
+        bdev.sector_count = 0;
+        bdev.sector_size = 0;
     }
     return bdev;
 }
 
 DRESULT disk_read(void *bdev_in, BYTE *buff, DWORD sector, UINT count) {
     struct _bdev_t *bdev = bdev_in;
-    FF_DEBUG("disk_read(%u, %u)\n", sector, count);
+    FF_DEBUG("-- disk_read(%u, %u)\n", sector, count);
     memcpy(buff, bdev->blocks + sector * bdev->sector_size, count * bdev->sector_size);
     return RES_OK;
 }
 
 DRESULT disk_write(void *bdev_in, const BYTE *buff, DWORD sector, UINT count) {
     struct _bdev_t *bdev = bdev_in;
-    FF_DEBUG("disk_write(%u, %u)\n", sector, count);
+    FF_DEBUG("-- disk_write(%u, %u)\n", sector, count);
     memcpy(bdev->blocks + sector * bdev->sector_size, buff, count * bdev->sector_size);
     bdev->used_bytes = MAX(bdev->used_bytes, (sector + count) * bdev->sector_size);
     return RES_OK;
 }
 
 DRESULT disk_ioctl (void *bdev_in, BYTE cmd, void* buff) {
-    FF_DEBUG("disk_ioctl(%d) -> ", cmd);
+    FF_DEBUG("-- disk_ioctl(%d) -> ", cmd);
     struct _bdev_t *bdev = bdev_in;
-    FF_DEBUG("count: %d size: %d\n", bdev->sector_count, bdev->sector_size);
 
     switch(cmd) {
         case CTRL_SYNC:           // 0   /* Complete pending write process (needed at FF_FS_READONLY == 0) */
@@ -64,7 +154,7 @@ DRESULT disk_ioctl (void *bdev_in, BYTE cmd, void* buff) {
             *((DWORD*)buff) = bdev->sector_count;
             break;
         case GET_SECTOR_SIZE:     // 2   /* Get sector size (needed at FF_MAX_SS != FF_MIN_SS) */
-            FF_DEBUG("GET_SECTOR_SIZE (%u)\n", bdev->sector_size);
+            FF_DEBUG("GET_SECTOR_SIZE (%d)\n", bdev->sector_size);
             *((WORD*)buff) = bdev->sector_size;
             break;
         case GET_BLOCK_SIZE:      // 3   /* Get erase block size (needed at FF_USE_MKFS == 1) */
@@ -90,44 +180,39 @@ DRESULT disk_ioctl (void *bdev_in, BYTE cmd, void* buff) {
 }
 
 DWORD get_fattime (void) {
-    FF_DEBUG("get_fattime\n");
+    FF_DEBUG("-- get_fattime()\n");
     return 0;
 }
 
-FATFS fs;
-FIL fil;
-FRESULT fr;
-
-char copy_buffer[1024];
-char copy_path[8192]; // Ugh
-char output_file[8192]; // Also ugh
-
 int copy_item(const char *filepath, const struct stat *info, const int typeflag, struct FTW *pathinfo)
 {
-    size_t read, written;
+    size_t read;  // Bytes read by system fread
+    UINT written; // Bytes written by FatFS
     char *targetpath = malloc(1024);
-    strcpy(targetpath, filepath + strlen(copy_path));
-    DEBUG("copy_item(%s -> %s, %d)\n", filepath, targetpath, typeflag);
-    if(typeflag == FTW_D) { // directory 
+    strcpy(targetpath, filepath + strlen(args.input_directory));
+
+    DEBUG("-- copy_item(%s -> %s, %d)\n", filepath, targetpath, typeflag);
+    if(typeflag == FTW_D) { // directory
         f_mkdir(&fs, targetpath);
-        printf("- mkdir %s\n", targetpath);
+        if(args.verbose) printf("- mkdir %s\n", targetpath);
     }
     if(typeflag == FTW_F) {
-        printf("- copy %s\n", targetpath);
+        if(args.verbose) printf("- copy %s\n", targetpath);
         struct stat st;
         stat(filepath, &st);
         size_t size = st.st_size;
-        DEBUG("File size: %u\n", size);
+        DEBUG("-- File size: %lu\n", size);
 
         FILE *f = fopen(filepath, "r");
         if(f == NULL) {
-            DEBUG("Failed to open source file!");
-            return 1;
+            fprintf(stderr, "\n⚠️    Failed to open %s for reading!\n", filepath);
+            exit(1);
         }
 
         fr = f_open(&fs, &fil, targetpath, FA_WRITE | FA_CREATE_NEW);
         if(fr != F_OK) {
-            DEBUG("Failed to create target file!");
+            fprintf(stderr, "\n⚠️    Failed to open %s for writing!\n", targetpath);
+            exit(1);
         }
 
         while(1) {
@@ -136,63 +221,68 @@ int copy_item(const char *filepath, const struct stat *info, const int typeflag,
             if(read == 0) {
                 break;
             }
-            f_write(&fil, copy_buffer, read, &written);
-            if(read != written) {
-                DEBUG("Write Error!");
+            fr = f_write(&fil, copy_buffer, read, &written);
+            if((UINT)read != written) {
+                fprintf(stderr, "\n⚠️    Write error copying %s (disk probably full!)\n", targetpath);
+                exit(1);
                 return 1;
             }
         }
         f_close(&fil);
-
-        //char *buf = malloc(size);
-        //fread(buf, 1, 1024, f);
     }
     return 0;
 }
 
 int main(int argc, char *argv[]) {
-    FILE *native_file;
-    printf("ffsmake 0.1\n"
-    "- MicroPython FatFS filesystem builder.\n"
-    "- usage: ffsmake source_dir output_file.bin\n");
+    args.verbose = true;
+    args.force = false;
+    args.debug = false;
+    args.truncate = true;
+    args.sector_count = DEFAULT_SECTOR_COUNT;
+    args.sector_size = DEFAULT_SECTOR_SIZE;
+    argp_parse(&argp, argc, argv, 0, 0, &args);
 
-    printf("\nAllocating 2MB disk...\n");
-    struct _bdev_t ramdisk = mkdisk(SECTOR_SIZE, (2 * 1024 * 1024) / SECTOR_SIZE);
+    VERBOSE("ffsmake 💾 -> 🐍\n");
+
+    if (access(args.output_file, F_OK) == 0 && !args.force) {
+        fprintf(stderr, "⚠️    Refusing to overwrite %s\n", args.output_file);
+        exit(1);
+    }
+
+    VERBOSE("\nAllocating %.2fMB disk...\n", (args.sector_count * args.sector_size) / MB);
+    struct _bdev_t ramdisk = mkdisk(args.sector_size, args.sector_count);
+    if(ramdisk.sector_count == 0) {
+        VERBOSE("- Failed!\n");
+        if (!args.verbose) fprintf(stderr, "⚠️    Failed to allocate disk!");
+        return 1;
+    }
     fs.drv = &ramdisk;
-    printf("- OK\n");
+    VERBOSE("- OK ✨\n");
 
-    printf("\nCreating filesystem...\n");
+    VERBOSE("\nCreating filesystem...\n");
     uint8_t working_buf[FF_MAX_SS];
     fr = f_mkfs(&fs, FM_FAT | FM_SFD, 0, working_buf, sizeof(working_buf));
     if (fr != FR_OK) {
-        printf("- Error %d\n", fr);
+        VERBOSE("- Error %d\n", fr);
+        if (!args.verbose) fprintf(stderr, "⚠️    Failed to create filesystem!");
         return 1;
     }
-    printf("- OK\n");
+    VERBOSE("- OK ✨\n");
 
-    printf("\nCopying files...\n");
-    if(argc >= 2) {
-        strcpy(copy_path, argv[1]);
-    } else {
-        strcpy(copy_path, "/Users/gadgetoid/Development/rp2350/august/august/badger2350/examples/badger_os");
-    }
-    printf("- using path: %s\n", copy_path);
-    nftw(copy_path, copy_item, 10, FTW_PHYS);
-    printf("- OK\n");
+    VERBOSE("\nCopying files...\n");
+    VERBOSE("- source directory: %s\n", args.input_directory);
+    nftw(args.input_directory, copy_item, 10, FTW_PHYS);
+    VERBOSE("- OK ✨\n");
 
 
-    printf("\nSaving filesystem...\n");
-    if(argc >= 3) {
-        strcpy(output_file, argv[2]);
-    } else {
-        strcpy(output_file, "filesystem.bin");
-    }
-    printf("- using path: %s\n", output_file);
-    native_file = fopen(output_file, "wb");
-    fwrite(ramdisk.blocks, 1, ramdisk.used_bytes, native_file);
+    VERBOSE("\nWriting FatFS binary...\n");
+    VERBOSE("- destination file: %s\n", args.output_file);
+    FILE *native_file = fopen(args.output_file, "wb");
+    size_t output_size = args.truncate ? ramdisk.used_bytes : (args.sector_count * args.sector_size);
+    fwrite(ramdisk.blocks, 1, output_size, native_file);
     fclose(native_file);
 
-    printf("- written: %lu bytes\n", ramdisk.used_bytes);
-    printf("- DONE!\n");
+    VERBOSE("- written: %.2f MB (of %.2f MB FatFS)\n", output_size / MB, args.sector_count * args.sector_size / MB);
+    VERBOSE("- DONE! 🎉\n");
     return 0;
 }
